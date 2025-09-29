bf26f6d0f72a1201115bcbb5e6dd7f43c79cdd48
Waile <wailenaeem22@gmail.com>
2025-09-29 15:59:55 +0500

    feat: add font loading and new assets for improved UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from 'react';
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded,error] = useFonts({
+    "QuickSand_Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "QuickSand_Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand_Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "QuickSand_SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "QuickSand_Light": require("../assets/fonts/Quicksand-Light.ttf")
+  })
+  console.log("error in line 14 in _layout.tsx", JSON.stringify(error, null, 2));
+  console.log("fontsLoaded in line 15 in _layout.tsx", JSON.stringify(fontsLoaded, null, 2));
+
+  useEffect(() => {
+    if(error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync()
+  }, [fontsLoaded,error])
+  
+
+  return <Stack screenOptions={{headerShown:false}} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
+import { getLoadedFonts } from "expo-font";
 import { Text, View } from "react-native";
 
 
 export default function Index() {
+  const fonts = getLoadedFonts()
+  console.log("loaded `fonts in line 7 in index.tsx", JSON.stringify(fonts, null, 2));
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-3xl text-center font-quicksand-bold" >
+        Welcome to Tailwind!
       </Text>
     </View>
   );
 }
+
+
+
